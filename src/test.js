bf6d82d6368120e177de19fccaae609e2a59b337
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-09-20 20:04:05 +0200

    Test for caesar chipher created

--- src/test.js
+++ src/test.js
@@ -1,6 +1,8 @@
 import { capitalize } from "./capitalize.js";
 import { reverseString } from "./reverseString.js";
 import { add, subtract, divide, multiply } from "./calculator.js";
+import { caesarCipher } from "./caesarCipher.js";
+
 test("Capitalize fist letter", () => {
   expect(capitalize("hello")).toBe("Hello");
 });
@@ -23,4 +25,12 @@ test("Divide calculation", () => {
 
 test("Multiply calculation", () => {
   expect(multiply(5, 2)).toBe(10);
+});
+
+test("Caesar chipher logic with key = + 3", () => {
+  expect(caesarCipher("JAVASCRIPT", 3)).toBe("MDYDVFULSW");
+});
+
+test("Caesar chipher logic with key = - 3", () => {
+  expect(caesarCipher("MDYDVFULSW.", -3)).toBe("JAVASCRIPT.");
 });
