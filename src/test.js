8d9142b17a60fe44429de30ab8649182cdc46242
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-09-20 18:51:34 +0200

    Test for reverse string created

--- src/test.js
+++ src/test.js
@@ -1,5 +1,10 @@
 import { capitalize } from "./capitalize.js";
+import { reverseString } from "./reverseString.js";
 
 test("Capitalize fist letter", () => {
   expect(capitalize("hello")).toBe("Hello");
+});
+
+test("Takes a string and returns it reversed", () => {
+  expect(reverseString("hello")).toBe("olleh");
 });
