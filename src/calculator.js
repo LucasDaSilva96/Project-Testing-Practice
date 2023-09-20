e54d9669ad200c1bcab14da4431a040cf698a5ac
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-09-20 19:24:41 +0200

    calculator file created  + add, subtract, divide and multiply functions

---
+++ src/calculator.js
@@ -0,0 +1,15 @@
+export function add(number_1, number_2) {
+  return number_1 + number_2;
+}
+
+export function subtract(number_1, number_2) {
+  return number_1 - number_2;
+}
+
+export function divide(number_1, number_2) {
+  return number_1 / number_2;
+}
+
+export function multiply(number_1, number_2) {
+  return number_1 * number_2;
+}
