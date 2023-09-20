1611b95c49b5fc4deb494f23fa45d8a0ebcf6b2e
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-09-20 19:25:21 +0200

    Test for different calculation created

--- src/test.js
+++ src/test.js
@@ -1,10 +1,26 @@
 import { capitalize } from "./capitalize.js";
 import { reverseString } from "./reverseString.js";
-
+import { add, subtract, divide, multiply } from "./calculator.js";
 test("Capitalize fist letter", () => {
   expect(capitalize("hello")).toBe("Hello");
 });
 
 test("Takes a string and returns it reversed", () => {
   expect(reverseString("hello")).toBe("olleh");
+});
+
+test("Add calculation", () => {
+  expect(add(2, 2)).toBe(4);
+});
+
+test("Subtract calculation", () => {
+  expect(subtract(2, 2)).toBe(0);
+});
+
+test("Divide calculation", () => {
+  expect(divide(10, 2)).toBe(5);
+});
+
+test("Multiply calculation", () => {
+  expect(multiply(5, 2)).toBe(10);
 });
